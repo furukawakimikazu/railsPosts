35a249808740d3df387c8cc69d3b98108479a78b
furukawakimikazu <g.guquan@gmail.com>
2020-05-27 12:40:47 +0900

    posts create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,12 +3,31 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
     @post = Post.new
   end
 
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
+    
+  end
+  
+
   def edit
   end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :body, :image)
+    end
+    
 end
