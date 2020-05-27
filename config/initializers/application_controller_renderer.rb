970c048ecf124e1e3cda203cd694ebd001b8535e
furukawakimikazu <g.guquan@gmail.com>
2020-05-27 10:39:56 +0900

    refile secret_keyをまたもや貼り付ける

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = '43c1aa3028740bb15abd1958304e6c870e5b1bb23d481d96681d8f58f6fbadcbc56967a0fa53d6911a64c034caf3ca94e19fdb71bf3877cdf5d310e76b551def'
