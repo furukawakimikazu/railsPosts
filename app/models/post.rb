ff1e57f3d0daf4b6af8aeb41a78a42e2f96fe64e
furukawakimikazu <g.guquan@gmail.com>
2020-05-30 16:11:55 +0900

    細かい調整

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   attachment :image
+  validates :title, presence: true
+  validates :body, presence: true,
+                      length: { maximum: 200 }
 end
