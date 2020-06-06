9629b00fcf5edb670d3b646de1cdef8abda43200
furukawakimikazu <g.guquan@gmail.com>
2020-06-06 14:48:38 +0900

    access limitation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
   before_action :find_post, only: [:edit, :update, :show, :destroy]
+  
   def index
     @posts = Post.all
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   before_action :find_params, only: [:show, :edit, :update]
 
   def index
